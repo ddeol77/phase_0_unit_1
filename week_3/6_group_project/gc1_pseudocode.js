87cfe8e178894b7224ffd454b2efe3042b6c3efd
ddeol77 <gurdevdeol@gmail.com>
2014-05-28 20:48:13 -0500

    Psuedocode

---
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -0,0 +1,31 @@
+// My role in the group is:
+// Person 2 - Translate user stories into pseudocode
+// Here is my part of the challenge:
+/*
+
+1.	-Create a function with name equal to Sum
+	-Set the parameter of Sum to an Array
+	-Set the Array length equal to odd or even
+	-Create a for loop inside the function to iterate through the Array
+	-Set a variable equal to a running total of the values in the Array
+	-Return the value of the variable in the function
+	-Call the function to get the Sum
+
+2.	-Create a function with name equal to Mean
+	-Set the parameter of Mean to an Array
+	-Set the Array length equal to odd or even
+	-Create a for loop inside the function to iterate through the Array
+	-Set a variable equal to a running total of the values in the Array 
+	 divided by the number of values in the Array
+	-Return the value of the variable in the function
+	-Call the function to get the Mean
+
+3.	-Create a function with name equal to Median
+	-Set the paramerameter of Median to an Array
+	-Set the Array length equal to odd or even
+	-Apply the sort method on the Array to pick the middle number
+	-If the Array has an odd number of values pick the middle number
+	-If the Array has an even number of values take the average of the two numbers in the middle
+	-Return this value in the funtion
+	-Call the function to get the Median
+
