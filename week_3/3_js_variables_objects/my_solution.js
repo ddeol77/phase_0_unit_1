ff330014f0bafd3852f76f302cd262330244c0c1
ddeol77 <gurdevdeol@gmail.com>
2014-05-31 13:01:48 -0500

    Committing ex 3

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,9 +1,11 @@
-// I paired [by myself, with:] on this challenge.
+// I paired with David Nanry on this challenge.
 
 // __________________________________________
 // Write your code below.
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John","David","Dev","Mary"];
 
 
 
@@ -12,13 +14,20 @@
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
+// 1. The problem we faced was being overwhelmed by the assert function initially.  What worked is that we took
+// each function call one at a time and understanding what the typeof and instanceof statements meant.
+// 2. I didn't understand what the typeof and instanceof operators did.  I used the mozilla developer network site
+// to learn what they did.
+// 3. I figured out this morning that object oriented programming can be done in Javascript.  I always thought it 
+// it was a client-side scripting language only. 
+// 4. I learned about test driven development and the importance of it.
+// 5. I'm very confident that I can define local variables in JavaScript, I'm moderately confident about creating  
+// objects of different types, very confident about creating and adding objects to an array, and moderately confident
+// about using pre-written test to drive development.
+// 6. The parts of the challenge I enjoyed the most were running the test and correcting the results by defining
+// new relevant variables.
+// I found it tedious at first to try and understand the function at the beginning of the program.
+//
 
 // __________________________________________
 // Test Code:  Do not alter code below this line.
