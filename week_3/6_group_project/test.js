853d2aa5e2f4e687f7e2a6e85beb3b2f8b320f7c
ddeol77 <gurdevdeol@gmail.com>
2014-05-31 13:02:24 -0500

    Ex 3

---
+++ week_3/6_group_project/test.js
@@ -0,0 +1,12 @@
+
+function Client(name, age, quote) {
+
+    this.name = name;
+    this.age = age; 
+    this.quote = quote;
+}
+ 
+var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
+console.log(shooterMcGavin.constructor === Client);
+console.log(shooterMcGavin.age === 48);
+console.log(shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.");
